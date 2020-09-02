5fd6b114cad64adcde26650e4b463f27456a5f07
deshxpm <deshdeepakxpm2@gmail.com>
2020-09-02 22:47:04 +0530

    quiz

---
+++ kidsmania/local_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'quiz',
+    'USER': 'root',
+    'PASSWORD': 'desh@1001',
+    'HOST': 'localhost',
+    'PORT': '3306',
+
+    }
+}
